c644fa29233b97a5e156d781bb73b94957e44283
Hery RAKOTOMANANA <122521311+Hery34@users.noreply.github.com>
2024-05-27 10:01:01 +0200

    V1.0.6

--- src/app/hery/page.js
+++ src/app/hery/page.js
@@ -54,7 +54,7 @@ export default function Hery() {
                             And I can also burst with colors
                         </h2>
                         <div className="flex items-center justify-center mt-4">
-                            <a href="mailto:me@hery.website" className="px-4 py-2 my-2 text-blue-800 uppercase bg-transparent border-2 border-blue-200 md:mt-16 dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 dark:text-white hover:bg-gray-800 hover:text-white text-md">
+                            <a href="mailto:me@hery.website" className="px-4 py-2 my-2 text-blue-800 uppercase bg-transparent border-2 border-blue-200 md:mt-16 dark:bg-white hover:dark:bg-gray-100 dark:text-white hover:bg-gray-800 hover:text-white text-md">
                                 CONNECT WITH ME
                             </a>
                         </div>
